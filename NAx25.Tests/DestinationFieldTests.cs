ec2e591bd367170ff8e6710ce37bc4d2321dc61c
Tom <tom@tomfanning.name>
2020-12-18 18:51:24 +0000

    Initial commit, very much WIP

---
+++ NAx25.Tests/DestinationFieldTests.cs
@@ -0,0 +1,109 @@
+﻿using Xunit;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Linq;
+
+namespace NAx25.Tests
+{
+    public class DestinationFieldTests
+    {
+        [Theory]
+        [InlineData(false, false)]
+        [InlineData(false, true)]
+        [InlineData(true, false)]
+        [InlineData(true, true)]
+        public void DestinationSubfieldDecoding(bool isLastAddress, bool cBit)
+        {
+            const bool r1 = true, r2 = true;
+            const byte ssid = 5;
+
+            var testData = new byte[] { 0xae, 0x84, 0x68, 0x94, 0x8c, 0x92, 0x00, GetA7Byte(isLastAddress, cBit, r1, r2, ssid) };
+
+            var target = new DestinationField(testData);
+
+            target.Callsign.Should().Be("WB4JFI");
+            target.CommandResponseBit.Should().Be(cBit);
+            target.ReservedBit1.Should().Be(r1);
+            target.ReservedBit2.Should().Be(r2);
+            target.Ssid.Should().Be(ssid);
+            target.IsLastAddress.Should().Be(isLastAddress);
+        }
+
+        [Fact]
+        public void NonrepeaterAx25FrameDecoding()
+        {
+            // fig 3a http://nic.vajn.icu/PDF/ham/AX25/ax25.html
+
+            var testData = new byte[] {
+                0x7e, // flag
+                0x96, // A1 = K
+                0x70, // A2 = 8
+                0x9a, // A3 = M
+                0x9a, // A4 = M
+                0x9e, // A5 = O
+                0x40, // A6 = space
+                0xe0, // A7 ssid = 11100000 = 0
+                0xae, // A8 = W
+                0x84, // A9 = B
+                0x68, // A10 = 4
+                0x94, // A11 = J
+                0x8c, // A12 = F
+                0x92, // A13 = I
+                0x61, // A14 ssid = 01100001 = 0 (last address)
+                0x3e, // Control = 00111110 = I
+                0xf0, // PID = 11110000 = none
+                0x00, // FCS1
+                0x00, // FCS2
+                0x7e, // flag
+            };
+
+            /* The frame shown is an I frame, 
+             * not going through a level 2 repeater, 
+             * from WB4JFI (SSID=0) 
+             * to K8MMO (SSID=0), 
+             * with no level 3 protocol. 
+             * The P/F bit is set; 
+             * the receive sequence number (N(R)) is 1; 
+             * the send sequence number (N(S)) is 7.
+             */
+
+            var iframe = new Frame(testData);
+
+            iframe.AddressBytes.Should().HaveCount(14); // from WB4JFI (SSID=0) to K8MMO(SSID = 0), not going through a level 2 repeater, 
+            iframe.DestinationAddress.Callsign.Should().Be("K8MMO");
+            iframe.DestinationAddress.Ssid.Should().Be(0);
+            iframe.SourceAddresses.Should().HaveCount(1);
+            iframe.SourceAddresses.First().Callsign.Should().Be("WB4JFI");
+            iframe.SourceAddresses.First().Ssid.Should().Be(0);
+            iframe.ControlByte.Should().Be(0x3e); // The P/F bit is set; the receive sequence number (N(R)) is 1; the send sequence number (N(S)) is 7.
+            iframe.FrameType.Should().Be(FrameType.Information); // The frame shown is an I frame, 
+            iframe.InformationFrameFields.Should().NotBeNull();
+            iframe.InformationFrameFields.ProtocolIdByte.Should().Be(0xf0); //  with no level 3 protocol. 
+            iframe.InformationFrameFields.InfoBytes.Should().BeNull();
+            iframe.FcsField.Should().Be(0);
+            
+        }
+
+        private static byte GetA7Byte(bool isLastAddress, bool cBit, bool r1, bool r2, byte ssid)
+        {
+            bool bit7 = cBit;
+            bool bit6 = r2;
+            bool bit5 = r1;
+            var ssidBitArray = new BitArray(ssid);
+            bool bit4 = ssidBitArray[3];
+            bool bit3 = ssidBitArray[2];
+            bool bit2 = ssidBitArray[1];
+            bool bit1 = ssidBitArray[0];
+            bool bit0 = isLastAddress;
+
+            var bitArray = new BitArray(new[] { bit7, bit6, bit5, bit4, bit3, bit2, bit1, bit0 });
+
+            byte[] bytes = new byte[1];
+            bitArray.CopyTo(bytes, 0);
+            return bytes[0];
+        }
+    }
+}
---
+++ NAx25/Frame.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NAx25
+{
+    public class ControlFields
+    {
+        public BitArray MBits { get; internal set; }
+        public bool PFBit { get; internal set; }
+    }
+
+    public class Frame
+    {
+        /// <summary>
+        /// http://nic.vajn.icu/PDF/ham/AX25/ax25.html#2.2.13
+        /// </summary>
+        public byte[] AddressBytes { get; set; }
+
+        /// <summary>
+        /// http://nic.vajn.icu/PDF/ham/AX25/ax25.html#2.3.2.1
+        /// </summary>
+        public byte ControlByte { get; set; }
+
+        public ControlFields ControlFields { get => Decoding.DecodeControlByte(ControlByte); }
+
+        private void SetControlFields(byte value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public InformationFrameFields InformationFrameFields { get; set; }
+
+        public UInt16 FcsField { get; set; }
+
+        public FrameType FrameType { get; set; }
+
+        public DestinationField DestinationAddress { get; }
+        public ICollection<SourceField> SourceAddresses { get; set; }
+
+        public SFrameType? SFrameType { get; set; }
+
+        public Frame(byte[] data)
+        {
+            var frameWithoutFlags = data.Where(b => b != 0x7e).ToArray();
+
+            byte[] theRest;
+            (AddressBytes, theRest) = Decoding.ConsumeAddressField(frameWithoutFlags);
+            (ControlByte, theRest) = Decoding.ConsumeByte(theRest);
+            FrameType = GetFrameType(ControlByte);
+
+            if (FrameType == FrameType.Information || FrameType == FrameType.UnnumberedInformation)
+            {
+                InformationFrameFields = new InformationFrameFields();
+                (InformationFrameFields.ProtocolIdByte, theRest) = Decoding.ConsumeByte(theRest);
+                (InformationFrameFields.InfoBytes, theRest) = Decoding.ConsumeInfoField(theRest);
+            }
+
+            if (FrameType == FrameType.Supervisory)
+            {
+                SFrameType = GetAx25V2SFrameType(AddressBytes);
+            }
+
+            (FcsField, theRest) = Decoding.ConsumeFcsField(theRest);
+
+            if (theRest.Length > 0)
+            {
+                throw new Exception("Frame decoding error - excess bytes");
+            }
+        }
+
+        private SFrameType GetAx25V2SFrameType(byte[] addressBytes)
+        {
+            if (addressBytes.Length != 14)
+            {
+                throw new NotSupportedException($"Cannot work out S-frame type with {addressBytes.Length} address field bytes, only 14");
+            }
+
+            var bitArray = new BitArray(addressBytes);
+            if (bitArray[14] == bitArray[7])
+            {
+                return NAx25.SFrameType.PreviousVersion;
+            }
+
+            if (bitArray[14] == true)
+            {
+                return NAx25.SFrameType.Command;
+            }
+
+            return NAx25.SFrameType.Response;
+        }
+
+        private FrameType GetFrameType(byte control)
+        {
+            var bitArray = new BitArray(control);
+
+            if (bitArray[0] && bitArray[1])
+            {
+                return FrameType.UnnumberedInformation;
+            }
+            else if (bitArray[0] && !bitArray[1])
+            {
+                return FrameType.Supervisory;
+            }
+            else if (!bitArray[0])
+            {
+                return FrameType.Information;
+            }
+
+            throw new ArgumentException($"Unknown frame type from {(bitArray[0] ? 1 : 0)},{(bitArray[1] ? 1 : 0)}", nameof(control));
+        }
+    }
+
+    public class InformationFrameFields
+    {
+        public byte ProtocolIdByte { get; set; }
+        public byte[] InfoBytes { get; set; }
+    }
+
+    public enum SFrameType
+    {
+        PreviousVersion, Command, Response
+    }
+
+    public enum FrameType
+    {
+        UnnumberedInformation, Information, Supervisory
+    }
+}
